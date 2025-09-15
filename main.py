9013c7e5a848fcb3281052810cf76c716546cc8b
Manbog <manbog@gmail.com>
2025-09-15 22:36:17 +0300

    Changed input to variable filepath, added books.

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+import sys
 from stats import (
     number_of_words,
     count_characters,
     sort_characters
 )
+
+if len(sys.argv) < 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
 
 def get_book_text(filepath):
     with open(filepath) as file:
@@ -10,7 +15,7 @@ def get_book_text(filepath):
     return file_contents
 
 def main():
-    book_path = "books/frankenstein.txt"
+    book_path = sys.argv[1]
     book_text = get_book_text(book_path)
     num_words = number_of_words(book_text)
     char_count = count_characters(book_text)
